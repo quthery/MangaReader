0438d0bfb765d639f50aa8cb4acd1386031ac3d5
par1tet <mankova486@gmail.com>
2024-06-05 14:06:51 +0300

    react upd

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from fastapi import UploadFile, File, HTTPException, FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse, PlainTextResponse
 from pathlib import Path
 import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory='static'), name="static")
 
-@app.get("/", response_class=HTMLResponse,)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+def main():
+    return RedirectResponse("http://localhost:3000/")
 
 @app.get("/manga/{manga_folder}", response_class=HTMLResponse)
 async def read_manga_page(request: Request, manga_folder: str):
@@ -23,4 +23,6 @@ async def read_manga_page(request: Request, manga_folder: str):
     )
 
 if __name__ == '__main__':
-    os.system(f"uvicorn main:app --port {8002} --reload")
+    os.system(f"uvicorn main:app --port {8003} --reload")
+    # cd react-app
+    # npm run start
