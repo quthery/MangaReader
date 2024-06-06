37385455b19d9ed5808a636ec82fd96917270203
par1tet <mankova486@gmail.com>
2024-06-06 13:44:48 +0300

    small changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import UploadFile, File, HTTPException, FastAPI, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 import os
@@ -19,19 +18,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get("/manga/{manga_folder}", response_class=HTMLResponse)
-async def read_manga_page(request: Request, manga_folder: str):
-    folder = Path('static/', manga_folder)
-    pages_number = sorted([str(img.name) for img in folder.iterdir()])
-    return templates.TemplateResponse(
-        "manga_page.html",
-        {"request": request, "manga_folder": manga_folder, "manga_images": pages_number},
-    )
     
 @app.get("/getMangePage/{id}")
 async def read_manga_page(request: Request, id: str):
-    print('123')
     return FileResponse(Path(f'static/manga/{id}.jpeg'))
 
 if __name__ == '__main__':
