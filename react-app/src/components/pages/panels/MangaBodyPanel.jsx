60a29a419e41386c3138a2524f10579a691f257a
par1tet <mankova486@gmail.com>
2024-06-07 22:56:11 +0300

    bg is work

--- react-app/src/components/pages/panels/MangaBodyPanel.jsx
+++ react-app/src/components/pages/panels/MangaBodyPanel.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './styles/MainPage.css';
+import FilterPanel from './FilterPanel.jsx';
+import axios from 'axios';
 
 function MangaBodyPanel(props) {
+    function showMangas(mangas){
+        console.log(mangas)
+    }
+
+    axios.get('http://127.0.0.1:8000/get_all')
+    .then(r => {
+        showMangas(r.data['Все манги'][0])
+    });
+
     return (
         <div className='body-panel'>
-            
+            <FilterPanel></FilterPanel>
         </div>
     );
 }
