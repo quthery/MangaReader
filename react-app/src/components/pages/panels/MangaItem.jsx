83c889a535bb955eb42641edd9e1f7ff21f4841f
par1tet <mankova486@gmail.com>
2024-06-11 14:22:13 +0300

    genius change

--- react-app/src/components/pages/panels/MangaItem.jsx
+++ react-app/src/components/pages/panels/MangaItem.jsx
@@ -9,14 +9,13 @@ const MangaItem = props => {
     }
 
     return (
-    <div className='manga-item'>
+    <div className='manga-item' onClick={() => navigationToReadPage(props.manga.name)}>
         <div className='manga-item-cover'>
             <img src='http://127.0.0.1:8000/jujika/1' alt='page manga'></img>
         </div>
         <div className='manga-item-info'>
             <div className='title'>
                 <span >Название: {props.manga.name}</span>
-                <button onClick={() => navigationToReadPage(props.manga.name)}><span>Перейти к чтению</span></button>
             </div>
             <div className='desc'>
                 <span>{`Описание: ${(props.manga.desc).slice(0,288)}`}</span>
